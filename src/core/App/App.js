bea5e916d8677e52eae98843ea713b8777b713b4
Szymon Basiul <szymon.basiul@wp.pl>
2023-07-23 15:43:47 +0200

    Normalize added, GlobalStyle connected

--- src/core/App/App.js
+++ src/core/App/App.js
@@ -1,8 +1,12 @@
 import PersonalHomepage from "../../features/personalHomepage";
+import { Normalize } from "styled-normalize";
+import { GlobalStyle } from "./GlobalStyle";
 
 function App() {
   return (
     <div>
+      <Normalize />
+      <GlobalStyle />
       <PersonalHomepage />
     </div>
   );
